705bec197e26976923d031a1ff8294b5b1326b19
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-30 12:31:42 +0200

    feat: draw octagon

--- 8lgen/main.go
+++ 8lgen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"os"
 
 	svg "github.com/ajstarks/svgo"
@@ -11,11 +12,23 @@ func main() {
 	height := 1024
 	canvas := svg.New(os.Stdout)
 	canvas.Start(width, height)
-	canvas.Circle(width/2, height/2, 100)
-	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+	drawOctagon(canvas, width/2, height/2, width*3/4/2)
+	canvas.Text(width/2, height/2, "Octalysis", "text-anchor:middle;font-size:30px;fill:white")
 	canvas.End()
 }
 
 func drawOctagon(canvas *svg.SVG, centerX int, centerY int, maxRadius int) {
+	canvas.Circle(centerX, centerY, maxRadius, "fill:none;stroke:black;stroke-width:2px;")
+	pointsX := []int{}
+	pointsY := []int{}
+	for i := 0; i < 8; i++ {
+		r := float64(maxRadius)
+		x := float64(centerX) + r*math.Cos(float64(2)*math.Pi*float64(i)/float64(8))
+		y := float64(centerY) + r*math.Sin(float64(2)*math.Pi*float64(i)/float64(8))
+
+		pointsX = append(pointsX, int(x))
+		pointsY = append(pointsY, int(y))
+	}
+	canvas.Polygon(pointsX, pointsY, "fill:blue;stroke:none;")
 
 }
